5004dc584e80e6b12dfa02bb06b61ee823007df9
AnicetMelachio <anicetmelachio@gmail.com>
2026-08-21 03:39:58 +0000

    feat: add public and private subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -15,3 +15,34 @@ resource "aws_internet_gateway" "main" {
     Name = "3tier-igw"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ca-central-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "3tier-public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_backend" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "ca-central-1a"
+
+  tags = {
+    Name = "3tier-private-backend-subnet"
+  }
+}
+
+resource "aws_subnet" "private_db" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ca-central-1a"
+
+  tags = {
+    Name = "3tier-private-db-subnet"
+  }
+}
