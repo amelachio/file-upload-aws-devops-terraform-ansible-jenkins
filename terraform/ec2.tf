77bfaed71153b116c1d3af5b8bcf68ea156b8371
AnicetMelachio <anicetmelachio@gmail.com>
2026-08-22 01:50:10 +0000

    feat: add SSH key pair and ubuntu AMI data source

---
+++ terraform/ec2.tf
@@ -0,0 +1,19 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws-3tier-key"
+  public_key = file("~/.ssh/aws-3tier-key.pub")
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
